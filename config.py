e8e1b142ff93ecc298a025d272e9d3ed50dcf0e4
Davy Peter Braun <hello@davybraun.com>
2014-12-31 12:34:54 +0100

    Set up DB migrations command, and models.

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'secret'
 
@@ -8,3 +13,6 @@ OPENID_PROVIDERS = [
   { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
   { 'name': 'MyOpenID', 'url': 'https://www.openid.com' }
 ]
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
