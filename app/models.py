e37498c04e86d3bdbcc90969175f5d8223a88bb1
Davy Peter Braun <hello@davybraun.com>
2015-01-02 16:44:52 +0100

    Profile pages and avatars.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from hashlib import md5
 
 
 class User(db.Model):
@@ -6,6 +7,26 @@ class User(db.Model):
   nickname = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
   posts = db.relationship('Post', backref='author', lazy='dynamic')
+  about_me = db.Column(db.String(140))
+  last_seen = db.Column(db.DateTime)
+
+  def is_authenticated(self):
+    return True
+
+  def is_active(self):
+    return True
+
+  def is_anonymous(self):
+    return False
+
+  def get_id(self):
+    try:
+      return unicode(self.id)
+    except NameError:
+      return str(self.id)
+
+  def avatar(self, size):
+    return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
 
   def __repr__(self):
     return '<User %r>' % (self.nickname)
