db68745f9e8b7a3cecbb34437de41dfdaf25ddef
Davy Peter Braun <hello@davybraun.com>
2015-01-02 17:10:31 +0100

    Fix typo in app init.

--- app/__init__.py
+++ app/__init__.py
@@ -27,7 +27,7 @@ if not app.debug:
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
-  file_handler = RotatingFIleHandler('tmp/microblog.log', 'a', 1*1024*1024, 10)
+  file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1*1024*1024, 10)
   file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
   app.logger.setLevel(logging.INFO)
   file_handler.setLevel(logging.INFO)
